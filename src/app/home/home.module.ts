a000abdfecf5aec42dd047661f4f35e051b6c090
Habib Syuhada <habibsumedang@gmail.com>
2020-02-08 14:16:45 +0700

    .

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 import { HomePage } from './home.page';
 
@@ -10,7 +13,9 @@ import { HomePage } from './home.page';
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
+    IonicModule,  
+    ScrollingModule,
+    DragDropModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Product } from '../product';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {
+export class HomePage implements OnInit {
+  ngOnInit(){
+    this.getProducts();
+  }
+  products: Product[] = [];
+  constructor(
+    public api: ApiService,
+    public loadingController: LoadingController,
+    public router: Router,
+    public route: ActivatedRoute
+  ) {
     
   }
+
+  async getProducts() {
+    const loading = await this.loadingController.create({
+      message: 'Loading...'
+    });
+    await loading.present();
+    await this.api.getProducts()
+    .subscribe(res => {
+      this.products = res;
+      console.log(this.products);
+      loading.dismiss();
+    }, err => {
+      console.log(err);
+      loading.dismiss();
+    });
+  }
+
+  drop(event: CdkDragDrop<any>) {
+    moveItemInArray(this.products, event.previousIndex, event.currentIndex);
+  }
+  
 }
