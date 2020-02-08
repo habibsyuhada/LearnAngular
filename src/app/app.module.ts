f5a3101d59b25c1514e55d1c3f547cd50a4ececd
Habib Syuhada <habibsumedang@gmail.com>
2020-02-08 15:41:47 +0700

    Revert "rest api init"
    This reverts commit 599f7e9274d3cf98a91c433d5218cd1c7890c823.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+<<<<<<< HEAD
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+=======
+import { FavouritePage } from './favourite/favourite.page';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+@NgModule({
+  declarations: [AppComponent, FavouritePage],
+  entryComponents: [FavouritePage],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, DragDropModule, ScrollingModule],
+>>>>>>> parent of 599f7e9... rest api init
   providers: [
     StatusBar,
     SplashScreen,
