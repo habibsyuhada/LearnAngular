37837466e97b071849b7edb810219ac4646ff18e
Habib Syuhada <habibsumedang@gmail.com>
2020-02-08 15:30:08 +0700

    set up json

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, retry } from 'rxjs/operators';
-import { Student } from 'models/student';
+import { Student } from './models/student';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
